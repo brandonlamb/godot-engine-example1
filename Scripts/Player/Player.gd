d8840409f025a80577b3027a84bb9d0c4265db82
Brandon Lamb <brandon@brandonlamb.com>
2016-06-03 15:11:56 -0700

    adjust camera and viewport

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -9,6 +9,7 @@ func _ready():
 
 func _fixed_process(delta):
 	var dir = Vector2()
+	var pos = get_pos()
 
 	if (Input.is_action_pressed("move_down")):
 		dir += Vector2(0, 1)
@@ -19,17 +20,16 @@ func _fixed_process(delta):
 	elif (Input.is_action_pressed("move_right")):
 		dir += Vector2(1, 0)
 
-	var pos = get_pos()
 	pos += dir * delta * SPEED
 
 	var size = get_viewport_rect().size
 	var size2 = get_viewport_rect().pos
-	var size3 = get_tree().get_node("/root").get_camera().get_size()
+	#var size3 = get_tree().get_node("/root").get_camera().get_size()
 	#var size3 = get_viewport().get_camera().get_size()
 	
 	#pos.x = clamp(pos.x, 0, size.x)
-	pos.x = clamp(pos.x, -size3.x, size3.x)
-	pos.y = clamp(pos.y, 0, size.y)
+	#pos.x = clamp(pos.x, -size3.x, size3.x)
+	#pos.y = clamp(pos.y, 0, size.y)
 
 	set_pos(pos)
 
