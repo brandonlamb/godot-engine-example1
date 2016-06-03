e570fccfc72e9c17763fb3cec911096ea03adf0e
Brandon Lamb <brandon@brandonlamb.com>
2016-06-03 12:51:12 -0700

    move left and right

---
+++ Scripts/Player/Player.gd
@@ -0,0 +1,31 @@
+
+extends Node2D
+
+export var speed = 100.0
+const SPEED = 100.0
+
+func _ready():
+	set_fixed_process(true);
+
+func _fixed_process(delta):
+	var dir = Vector2()
+
+	if (Input.is_action_pressed("move_down")):
+		dir += Vector2(0, 1)
+	elif (Input.is_action_pressed("move_up")):
+		dir += Vector2(0, -1)
+	elif (Input.is_action_pressed("move_left")):
+		dir += Vector2(-1, 0)
+	elif (Input.is_action_pressed("move_right")):
+		dir += Vector2(1, 0)
+
+	var pos = get_pos()
+	pos += dir * delta * SPEED
+
+	var size = get_viewport_rect().size
+	pos.x = clamp(pos.x, 0, size.x)
+	pos.y = clamp(pos.y, 0, size.y)
+	set_pos(pos)
+
+#	var rect = get_viewport_rect()
+#	translate(dir * delta * SPEED)
