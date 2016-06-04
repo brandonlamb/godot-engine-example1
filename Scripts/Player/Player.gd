6952d0f3418d1261a95328ab89eafcc68a2ce20d
Brandon Lamb <brandon@brandonlamb.com>
2016-06-03 18:32:34 -0700

    camera follow

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -2,20 +2,28 @@
 extends Node2D
 
 export var speed = 100.0
-#const SPEED = 100.0
+
+const __EDGE_BUFFER = 400.0
+
+var __root
 var __level_width
 var __level_height
+var __camera
 
 func _ready():
-	var root = get_tree().get_root().get_node("SceneRoot")
-	__level_width = root.level_width
-	__level_height = root.level_height
+	__root = get_tree().get_root().get_node("SceneRoot")
+	__camera = get_node("Camera2D")
+	__level_width = __root.level_width
+	__level_height = __root.level_height
 	
 	set_fixed_process(true);
 
 func _fixed_process(delta):
 	var dir = Vector2()
 	var pos = get_pos()
+	var cam = Vector2()
+	var x = 0
+	var y = 0
 	var left = Input.is_action_pressed("move_left")
 	var right = Input.is_action_pressed("move_right")
 	var up = Input.is_action_pressed("move_up")
@@ -37,23 +45,26 @@ func _fixed_process(delta):
 		dir += Vector2(1, 0)
 
 	pos += dir * delta * speed
-
-	#var size = get_viewport_rect().size
-	#var size2 = get_viewport_rect().pos
-	#var size3 = get_tree().get_node("/root").get_camera().get_size()
-	#var size3 = get_viewport().get_camera().get_size()
-	#var bg = get_node("/root/SceneRoot/Background").get_region_rect()
-	
-	#print("LEVEL_WIDTH: ", __level_width, "\nLEVEL_HEIGHT: ", __level_height)
-	
 	pos.x = clamp(pos.x, -__level_width, __level_width)
 	pos.y = clamp(pos.y, -__level_height, __level_height)
-	#pos.x = clamp(pos.x, 0, size.x)
-	#pos.x = clamp(pos.x, -size3.x, size3.x)
-	#pos.y = clamp(pos.y, 0, size.y)
 
-	print(pos)
+	# Prevent camera from going past edge of level
+	var diff_left = -__level_width - pos.x
+	var diff_right = __level_width - pos.x
+	if (diff_left < -__EDGE_BUFFER || diff_right > __EDGE_BUFFER):
+		x = clamp(pos.x, -__level_width + __EDGE_BUFFER, __level_width - __EDGE_BUFFER)
+		cam.x = x
+
+	var diff_up = -__level_height - pos.y
+	var diff_down = __level_height - pos.y
+	if (diff_up < -__EDGE_BUFFER || diff_down > __EDGE_BUFFER):
+		y = clamp(pos.y, -__level_height + __EDGE_BUFFER, __level_height - __EDGE_BUFFER)
+		cam.y = y
+
+	print("POS: ", pos)
+	print("CAM: ", x, ",", y)
 	set_pos(pos)
+	__camera.set_pos(cam)
 
 #	var rect = get_viewport_rect()
 #	translate(dir * delta * SPEED)
