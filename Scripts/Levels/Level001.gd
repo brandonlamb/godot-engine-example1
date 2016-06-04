df0347b2c29c02cce05bd989f4f11ec6d7d5ea30
Brandon Lamb <brandon@brandonlamb.com>
2016-06-03 17:59:31 -0700

    Clamp the camera to level limits

--- Scripts/Levels/Level001.gd
+++ Scripts/Levels/Level001.gd
@@ -1,5 +1,8 @@
 
 extends Node2D
 
+export var level_width = 3072
+export var level_height = 2048
+
 func _ready():
 	pass
--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -3,20 +3,26 @@ extends Node2D
 
 export var speed = 100.0
 #const SPEED = 100.0
-var background_node
+var __level_width
+var __level_height
 
 func _ready():
-	background_node = get_tree().get_root().get_node("/root/SceneRoot/Background")
+	var root = get_tree().get_root().get_node("SceneRoot")
+	__level_width = root.level_width
+	__level_height = root.level_height
+	
 	set_fixed_process(true);
 
 func _fixed_process(delta):
 	var dir = Vector2()
 	var pos = get_pos()
-	var bg = get_node("/root/SceneRoot/Background")
 	var left = Input.is_action_pressed("move_left")
 	var right = Input.is_action_pressed("move_right")
 	var up = Input.is_action_pressed("move_up")
 	var down = Input.is_action_pressed("move_down")
+
+	if (!(left or right or up or down)):
+		return
 
 	if (up):
 		dir += Vector2(0, -1)
@@ -32,17 +38,21 @@ func _fixed_process(delta):
 
 	pos += dir * delta * speed
 
-	var size = get_viewport_rect().size
-	var size2 = get_viewport_rect().pos
+	#var size = get_viewport_rect().size
+	#var size2 = get_viewport_rect().pos
 	#var size3 = get_tree().get_node("/root").get_camera().get_size()
 	#var size3 = get_viewport().get_camera().get_size()
+	#var bg = get_node("/root/SceneRoot/Background").get_region_rect()
 	
+	#print("LEVEL_WIDTH: ", __level_width, "\nLEVEL_HEIGHT: ", __level_height)
 	
-	#pos.x = clamp()
+	pos.x = clamp(pos.x, -__level_width, __level_width)
+	pos.y = clamp(pos.y, -__level_height, __level_height)
 	#pos.x = clamp(pos.x, 0, size.x)
 	#pos.x = clamp(pos.x, -size3.x, size3.x)
 	#pos.y = clamp(pos.y, 0, size.y)
 
+	print(pos)
 	set_pos(pos)
 
 #	var rect = get_viewport_rect()
